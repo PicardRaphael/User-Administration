8c04296ce95742474ff3e19be1958dfc4f075b4f
Raphaël PICARD <raphaelpicard@ymail.com>
2021-12-07 14:58:11 +0100

    Added EDIT_USER

--- src/store/reducers/UsersReducer.js
+++ src/store/reducers/UsersReducer.js
@@ -12,6 +12,21 @@ const UsersReducer = (state, action) => {
           return user.id !== action.payload;
         })
       }
+    case 'EDIT_USER':
+      const updateUser = action.payload;
+
+      const updateUsers = state.users.map(user => {
+        if (user.id === updateUser.id)
+        {
+          console.log('ok')
+          return updateUser;
+        }
+        return user;
+      })
+      return {
+        ...state,
+        users: updateUsers
+      }
     default:
       return state;
   }
